26c991f5a11d4273df9c352988a91a1820595138
Matteo Barone <matteobarone90@alice.it>
2018-07-31 10:12:43 +0200

    Revert "refactor(modal): modal in app component"
    This reverts commit 2e71bafb2bb7133ad043464ab4eb17a45f4669da.

--- packages/ng-hero-animation/src/app/app.component.js
+++ packages/ng-hero-animation/src/app/app.component.js
@@ -23,26 +23,16 @@ const template = `
     z-index: 10000;
 }
 .modal {
-    z-index: 20;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 50%;
     height: 50%;
-    background-color: #eeeeee;
+    background-color: green;
     display: flex;
-    justify-content: flex-end;
-    padding: 10px;
-    box-shadow: 0 5px 30px 0 rgba(97,97,97,1);
-}
-.backdrop-modal {
-    z-index: 15;
-    background-color: rgba(0,0,0,.3);
-    position: absolute;
-    top: 0;
-    width: 100vw;
-    height: 100vh;
+    justify-content: center;
+    align-items: center;
 }
 .modal.ng-enter {
   transition: opacity 500ms ease-out;
@@ -63,46 +53,19 @@ const template = `
   <header>
     <nav>
       <a ng-repeat="link in $ctrl.links" ui-sref="{{link.state}}">{{link.label}}</a>
-      <a ng-click="$ctrl.showModal()">Show Modal</a>
     </nav>
   </header>
   <div ui-view class="hero-animation"></div>
 </main>
 `;
 
-const modalTemplate = `
-  <div class="modal hero-animation">
-    <div class="polymer--modal" hero-id="modal"></div>
-    <span ng-click="$ctrl.modalClose()">close</span>
-  </div>
-  <div class="backdrop-modal"></div>
-`;
-
-
 class Controller {
-  static get $inject() {
-    return ['ModalService'];
-  }
-
-  constructor(ModalService) {
-    this.modalService = ModalService;
-  }
-
   $onInit() {
     this.links = [
       {state: 'moduleOne', label: 'One'},
       {state: 'moduleTwo', label: 'Two'},
       {state: 'moduleThree', label: 'Three'},
     ];
-  }
-
-  showModal() {
-    this.modal = this.modalService.showModal({
-        template: modalTemplate,
-        controller: 'ModalController as $ctrl',
-      })
-      .then((modal) => modal.close)
-      .then(() => this.modal = null);
   }
 }
 
--- packages/ng-hero-animation/src/app/module-three/module-three.component.js
+++ packages/ng-hero-animation/src/app/module-three/module-three.component.js
@@ -2,7 +2,6 @@ const template = `
 <div class="view" style="background: lightblue">
     <h1 class="title--three" hero-id="title">{{$ctrl.title}}</h1>
     <div class="polymer--three" hero-id="polymer"></div>
-    <div class="polymer--modal__view" hero-id="modal"></div>
 </div>
 `;
 
